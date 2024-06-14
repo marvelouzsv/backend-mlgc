a3b0e0284c0cc6803e49aae1d411b9926626aaeb
Schatzy <95327147+marvelouzsv@users.noreply.github.com>
2024-06-14 23:44:46 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -3,11 +3,11 @@ import { Firestore } from '@google-cloud/firestore';
 const storeData = async (id, data) => {
   const db = new Firestore({
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    projectId: 'submissionmlgc-tiara-424716',
-    databaseId: 'predictions',
+    projectId: 'your-project-id',
+    databaseId: 'your-project-database',
   });
 
-  const predictCollection = db.collection('predictions');
+  const predictCollection = db.collection('your-project-collection');
 
   return predictCollection.doc(id).set(data);
 };
