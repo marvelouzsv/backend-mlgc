7ebc9544366eeee7a4cfe9f952ca0734e3aabcd1
Schatzy <95327147+marvelouzsv@users.noreply.github.com>
2024-06-14 23:45:34 +0700

    Update loadHistoryData.js

--- src/services/loadHistoryData.js
+++ src/services/loadHistoryData.js
@@ -3,11 +3,11 @@ import { Firestore } from '@google-cloud/firestore';
 const loadHistoryData = async () => {
   const db = new Firestore({
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    projectId: 'submissionmlgc-tiara-424716',
-    databaseId: 'predictions',
+    projectId: 'your-project-id',
+    databaseId: 'your-project-database-id',
   });
 
-  const predictCollection = db.collection('predictions');
+  const predictCollection = db.collection('your-project-collection');
 
   const snapshot = await predictCollection.orderBy('createdAt', 'desc').get();
 
